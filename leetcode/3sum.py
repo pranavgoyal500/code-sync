3a82e98dc36b780f078a638b267ce3f664d65244
pranavgoyal500 <pranavgoyal500@gmail.com>
2025-10-25 15:13:59 +0530

    Add solution for 3sum

---
+++ leetcode/3sum.py
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        int n = nums.length;
+        if (n < 3) {
+            return result;
+        }
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) { // skip duplicate i
+                continue;
+            }
+
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+
+                    // skip duplicates for j and k
+                    while (j < k && nums[j] == nums[j + 1]) j++;
+                    while (j < k && nums[k] == nums[k - 1]) k--;
+
+                    j++;
+                    k--;
+                }
+            }
+        }
+
+        return result;
+    }
+}
